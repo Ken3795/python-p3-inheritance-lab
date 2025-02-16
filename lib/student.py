859dcdf01bf7dac7bb8c0bdadf729a576ef8f404
Kenneth Kipkoech <kenkipkoech57@gmail.com>
2025-02-16 15:58:57 +0300

    lab completed

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        # Initialize the parent class User with first and last name
+        super().__init__(first_name, last_name)
+        # Initialize knowledge with an empty list
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, info):
+        # Add learned information to the knowledge list
+        self.knowledge.append(info)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+        # Initialize the parent class User with first and last name
+        super().__init__(first_name, last_name)
+        # Initialize knowledge with some default topics
+        self.knowledge = ["Math", "Science", "History", "Literature"]
+    
     def teach(self):
-        pass
+        # Return a random topic from the knowledge list
+        return random.choice(self.knowledge)
